5a2ab85e24f1edf20c30b47aa54e0e1f7c88a0f8
MicheleCasani <casanimichele@gmail.com>
2025-05-20 15:06:12 +0200

    [UPD] add img

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -20,12 +20,15 @@ const MovieCard = ({ movie }) => {
         pl: 'pl',
     };
 
+    // variabile che contiene  il path delle copertine dei film/serie
+    const imagePath = `https://image.tmdb.org/t/p/w200${movie.poster_path}`
 
     // recupero il codice della bandiera corrispondente alla lingua del film/serie
     const countryFlag = languageFlags[movie.original_language];
 
     return (
         <div>
+            <img src={imagePath} alt="" />
             <h3>{movie.title || movie.name}</h3>
             <p><strong>Titolo originale:</strong> {movie.original_title || movie.original_name}</p>
             <p>
